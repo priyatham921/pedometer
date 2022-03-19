a454fb64a5f115bf22037340d2a9d40307302b31
priyatham921 <hiddencloud158@gmail.com>
2022-03-19 08:18:58 +0530

    last edit

--- pedometer.py
+++ pedometer.py
@@ -6,6 +6,10 @@ columns = ["Time","Sensor1","Sensor2","Sensor3"]
 accelerometer = pd.read_csv("accelerometer1.csv", usecols=columns)
 
 plt.plot(accelerometer.Time, accelerometer.Sensor1)
+plt.plot(accelerometer.Time, accelerometer.Sensor2)
+plt.plot(accelerometer.Time, accelerometer.Sensor3)
+
+
 
 plt.show()
 
