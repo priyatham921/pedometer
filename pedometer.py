463cccb8ee8e94746fe68377fb2bb4529546ab23
priyatham921 <hiddencloud158@gmail.com>
2022-05-21 12:52:13 +0530

    plotting data

--- pedometer.py
+++ pedometer.py
@@ -3,7 +3,7 @@ from matplotlib import pyplot as plt
 import pandas as pd
 
 columns = ["Time","Sensor1","Sensor2","Sensor3"]
-accelerometer = pd.read_csv("accelerometer1.csv", usecols=columns)
+accelerometer = pd.read_csv("curls1.csv", usecols=columns)
 
 plt.plot(accelerometer.Time, accelerometer.Sensor1)
 plt.plot(accelerometer.Time, accelerometer.Sensor2)
