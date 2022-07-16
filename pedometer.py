8f557262f09066ccbf529c180533a15afbb2ca07
priyatham921 <hiddencloud158@gmail.com>
2022-07-16 10:06:28 +0530

    added reps.csv

--- pedometer.py
+++ pedometer.py
@@ -1,13 +1,16 @@
 from matplotlib import pyplot as plt
-
 import pandas as pd
+from scipy.signal import find_peaks_cwt
+import numpy as np
 
 columns = ["Time","Sensor1","Sensor2","Sensor3"]
-accelerometer = pd.read_csv("curls1.csv", usecols=columns)
+accelerometer = pd.read_csv("reps.csv", usecols=columns)
 
-plt.plot(accelerometer.Time, accelerometer.Sensor1)
-plt.plot(accelerometer.Time, accelerometer.Sensor2)
-plt.plot(accelerometer.Time, accelerometer.Sensor3)
+data = np.array(accelerometer.Sensor1)
+peaks = find_peaks_cwt(data, widths=np.ones(data.shape)*2)-1
+plt.plot(data)
+
+plt.plot(peaks, data[peaks], "x")
 
 
 
